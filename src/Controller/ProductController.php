9dc2c738e756225abfc7e542878dd1b0585fcf5e
aymen2782 <aymen.sellaouti@gmail.com>
2021-05-08 10:49:50 +0100

    form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\ProductType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -27,48 +29,44 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/add/{name}/{description}/{price<\d+>}", name="product.add")
+     * @Route("/add/{product?0}", name="product.add")
      */
-    public function addProduct($name, $description, $price, EntityManagerInterface $manager) {
-        echo 'in add';
-//        $manager = $this->getDoctrine()->getManager();
-        $product = new Product();
-        $product->setName($name);
-        $product->setDescription($description);
-        $product->setPrice($price);
-//
-//        $product2 = new Product();
-//        $product2->setName($name);
-//        $product2->setDescription($description);
-//        $product2->setPrice($price);
-
-        $manager->persist($product);
-//        $manager->persist($product2);
-        $manager->flush();
-
-        return $this->render('product/detail.html.twig', [
-            'product' => $product
-        ]);
-    }
-
-    /**
-     * @Route("/update/{product}/{name}/{description}/{price<\d+>}", name="product.update")
-     */
-    public function updateProduct(Product $product = null, $name, $description, $price, EntityManagerInterface $manager) {
-        if ($product) {
-            $product->setName($name);
-            $product->setDescription($description);
-            $product->setPrice($price);
-
-            $manager->persist($product);
-//        $manager->persist($product2);
-            $manager->flush();
+    public function addProduct(EntityManagerInterface $manager, Request  $request, Product $product = null) {
+        if(!$product) {
+            $product = new Product();
         }
 
-        return $this->render('product/detail.html.twig', [
-            'product' => $product
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+        if($form->isSubmitted()) {
+          $manager->persist($product);
+          $manager->flush();
+          $this->addFlash('success', "le produit ".$product->getName()." a été ajouté avec succès");
+          return $this->redirectToRoute('product.list');
+        }
+        return $this->render('product/add.html.twig', [
+            'form' => $form->createView()
         ]);
     }
+
+//    /**
+//     * @Route("/update/{product}/{name}/{description}/{price<\d+>}", name="product.update")
+//     */
+//    public function updateProduct(Product $product = null, $name, $description, $price, EntityManagerInterface $manager) {
+//        if ($product) {
+//            $product->setName($name);
+//            $product->setDescription($description);
+//            $product->setPrice($price);
+//
+//            $manager->persist($product);
+////        $manager->persist($product2);
+//            $manager->flush();
+//        }
+//
+//        return $this->render('product/detail.html.twig', [
+//            'product' => $product
+//        ]);
+//    }
     /**
      * @Route("/delete/{product}", name="product.delete")
      */
