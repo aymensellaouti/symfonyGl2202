8c61a37526fa707535f43516a6aac79bb08e908d
aymen2782 <aymen.sellaouti@gmail.com>
2021-07-26 17:05:39 +0100

    event 2

--- src/EventSubscriber/TestSubscriber.php
+++ src/EventSubscriber/TestSubscriber.php
@@ -4,6 +4,7 @@ namespace App\EventSubscriber;
 
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 class TestSubscriber implements EventSubscriberInterface
@@ -14,13 +15,24 @@ class TestSubscriber implements EventSubscriberInterface
     public function onKernelRequest(RequestEvent $event)
     {
         //dd($event);
+        dump($event->getRequest());
+        $this->logger->info("Je viens de créer mon premier subscriber");
+        if($event->getRequest()->getLocale()==='fr') {
+            $event->stopPropagation();
+        }
+    }
+
+    public function onKernelController(ControllerEvent $event)
+    {
+        dd($event->getRequest());
         $this->logger->info("Je viens de créer mon premier subscriber");
     }
 
     public static function getSubscribedEvents()
     {
         return [
-           RequestEvent::class => 'onKernelRequest',
+           RequestEvent::class => ['onKernelRequest',-1024],
+           ControllerEvent::class => ['onKernelController',-1024]
         ];
     }
 }
